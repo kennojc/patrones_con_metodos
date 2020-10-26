69a0edaa6e52f4c39c420a03bd7a6f7534f66308
Kenneth Johnson C <kennojc@gmail.com>
2020-10-26 00:19:48 -0300

    add letra O

---
+++ patrones.rb
@@ -0,0 +1,17 @@
+n = ARGV[0].to_i
+
+def letra_o (n)
+    puts "\n"
+    print "*" * n
+    print "\n"
+    (n-2).times do |i|
+        print "*"
+        print " " * (n-2)
+        print "*"
+        print "\n"
+    end
+    print "*" * n
+end
+
+puts letra_o(n)
+puts "\n"
