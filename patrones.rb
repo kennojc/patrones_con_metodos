db65d97678f148f9a3a9c6f91d8794fb98519e37
Kenneth Johnson C <kennojc@gmail.com>
2020-10-26 20:13:53 -0300

    add X

--- patrones.rb
+++ patrones.rb
@@ -40,6 +40,23 @@ def letra_z (n)
 end
 
 
+def letra_x (n)
+    puts "\n"
+    for i in (1..n) do
+        for j in 1..n do
+            if i != j &&  j != (n+1)-i
+                print " "
+            else
+                print "*"
+            end
+        end
+        print "\n"
+    end 
+    puts "\n"   
+end
+
 puts letra_o(n)
 puts letra_i(n)
 puts letra_z(n)
+puts letra_x(n)
+
