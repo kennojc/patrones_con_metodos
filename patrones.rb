334dea5f2d1e778731d7495d1a05f0c54349beba
Kenneth Johnson C <kennojc@gmail.com>
2020-10-26 01:09:28 -0300

    cambio I y add Z

--- patrones.rb
+++ patrones.rb
@@ -18,9 +18,22 @@ def letra_i (n)
     print "*" * n
     print "\n"
     (n-2).times do |i|
-        print "  "
+        print " " * (n/2)
         print "*"
-        print "  "
+        print "\n"
+    end
+    print "*" * n
+end
+
+def letra_z (n)
+    puts "\n"
+    print "*" * n
+    print "\n"
+    (n-2).times do |i|
+        ((n-2)-i).times do
+        print " "
+        end
+        print "*" 
         print "\n"
     end
     print "*" * n
@@ -29,3 +42,4 @@ end
 
 puts letra_o(n)
 puts letra_i(n)
+puts letra_z(n)
